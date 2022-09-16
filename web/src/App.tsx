f99712453075cb2b2c96cece100c6f837c0160b7
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2022-09-16 00:39:56 -0300

    Home screen page styles completed

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,42 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
+import logoImg from "./assets/logo-nlw-esports.svg"
+
+import {GameBanner} from './components/GameBanner'
+
+import './styles/main.css'
+
 function App() {
-  return <h1>Hello NLW</h1>
+  return (
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className="text-transparent bg-nlw-gradient bg-clip-text">duo</span> esta aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <GameBanner />
+        <GameBanner />
+        <GameBanner />
+        <GameBanner />
+        <GameBanner />
+      </div>
+      <div className="pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
+          <div>
+          <strong className="text-2xl text-white font-black">Nao encontrou seu duo</strong>
+          <span className="text-zinc-400 block">Publique um anuncio para encontrar novos players</span>
+          </div>
+
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anuncio
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ web/src/components/GameBanner.tsx
@@ -0,0 +1,13 @@
+
+export function GameBanner() {
+    return (
+        <a href="" className="relative rounded-lg overflow-hidden">
+        <img src="" alt="" />
+
+        <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+          <strong className="font-bold text-white block">League of Legengs</strong>
+          <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+        </div>
+      </a>
+    )
+}
