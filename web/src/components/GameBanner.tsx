80287db880e7b5722dba21766bc568aea09aabce
Murilo Lodovico <37813008+Mlodovico@users.noreply.github.com>
2022-09-17 12:20:13 -0300

    using api and popup component finished

--- web/src/components/GameBanner.tsx
+++ web/src/components/GameBanner.tsx
@@ -1,12 +1,17 @@
+interface GameBannerProps {
+  bannerUrl: string;
+  title: string;
+  adsCount: number;
+}
 
-export function GameBanner() {
+export function GameBanner(props: GameBannerProps) {
     return (
-        <a href="" className="relative rounded-lg overflow-hidden">
-        <img src="" alt="" />
+      <a href="" className="relative rounded-lg overflow-hidden">
+        <img src={props.bannerUrl} alt="" />
 
         <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
-          <strong className="font-bold text-white block">League of Legengs</strong>
-          <span className="text-zinc-300 text-sm block mt-1">4 anuncios</span>
+          <strong className="font-bold text-white block">{props.title}</strong>
+          <span className="text-zinc-300 text-sm block mt-1">{props.adsCount} anuncio(s)</span>
         </div>
       </a>
     )
